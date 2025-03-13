843caec18e6f604f7151c08b70eb77a3ff756e94
Yiren Liang <yiren.liang@kcl.ac.uk>
2025-03-13 18:28:17 +0000

    IBclient class to create client and error function - Iyen

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-for i in range(9):
-    print("hi")
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+
+from threading import Thread
+
+class IBClient(EWrapper, EClient):
+
+    def __init__(self, host, port, client_id):
+        EClient.__init__(self,self)
+
+        self.connect(host, port, client_id)
+        thread = Thread(target=self.run)
+        thread.start()
+
+    def error(self, req_id, code, msg, misc):
+        if code in [2104, 2106, 2158]:
+            print(msg)
+        else:
+            print('Error{}:{}'.format(code, msg))
+
+client = IBClient('127.0.0.1', 7497, 1)
