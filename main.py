e3d18c3d9bce34929552b76c4a7f42e5a192389b
Yiren Liang <yiren.liang@kcl.ac.uk>
2025-03-14 17:11:29 +0000

    download stock data - Iyen

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
+import time, datetime
+from ibapi.client import Contract
 
 from threading import Thread
+
+init_symbol = "TSLA"
+
+default_host = '127.0.0.1'
+default_client_id = 1
+paper_trading_port = 7497
+live_trading_port = 7496
+live_trading = False
+trading_port = paper_trading_port
+if live_trading:
+    trading_port = live_trading_port
 
 class IBClient(EWrapper, EClient):
 
@@ -18,4 +31,24 @@ class IBClient(EWrapper, EClient):
         else:
             print('Error{}:{}'.format(code, msg))
 
-client = IBClient('127.0.0.1', 7497, 1)
+    def historicalData(self, req_id, bar):
+        print(bar)
+
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        print(f"end of data {start} {end}")
+
+
+
+client = IBClient(default_host,trading_port,default_client_id)
+time.sleep(1)
+contract = Contract()
+contract.symbol = 'TSLA'
+contract.secType = 'STK'
+contract.exchange = 'SMART'
+contract.currency = 'USD'
+what_to_show = 'TRADES'
+
+client.reqHistoricalData(
+    2, contract, '', '30 D', '5 mins', what_to_show, True, 2, False, []
+)
+time.sleep(1)
