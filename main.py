51f1d122c22ba441476c06cd69b4390ba838404a
Yiren Liang <yiren.liang@kcl.ac.uk>
2025-03-14 22:07:17 +0000

    download stock data - Iyen

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+import queue
+import pandas as pd
+
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 import time, datetime
 from ibapi.client import Contract
 
+from lightweight_charts import Chart
+
 from threading import Thread
 
 init_symbol = "TSLA"
+init_timeframe = '5 mins'
 
 default_host = '127.0.0.1'
 default_client_id = 1
@@ -15,6 +21,9 @@ live_trading = False
 trading_port = paper_trading_port
 if live_trading:
     trading_port = live_trading_port
+
+
+data_queue = queue.Queue()
 
 class IBClient(EWrapper, EClient):
 
@@ -34,21 +43,75 @@ class IBClient(EWrapper, EClient):
     def historicalData(self, req_id, bar):
         print(bar)
 
+
+        t = datetime.datetime.fromtimestamp(int(bar.date))
+
+        # creation bar dictionary for each bar received
+        data = {
+            'date': t,
+            'open': bar.open,
+            'high': bar.high,
+            'low': bar.low,
+            'close': bar.close,
+            'volume': int(bar.volume)
+        }
+
+        print(data)
+
+        # Put the data into the queue
+        data_queue.put(data)
+
     def historicalDataEnd(self, reqId: int, start: str, end: str):
         print(f"end of data {start} {end}")
 
+        update_chart()
 
+def update_chart():
+    try:
+        bars = []
+        while True:  # Keep checking the queue for new data
+            data = data_queue.get_nowait()
+            bars.append(data)
+    except queue.Empty:
+        print("empty queue")
+    finally:
+        # once we have received all the data, convert to pandas dataframe
+        df = pd.DataFrame(bars)
+        print(df)
 
-client = IBClient(default_host,trading_port,default_client_id)
-time.sleep(1)
-contract = Contract()
-contract.symbol = init_symbol
-contract.secType = 'STK'
-contract.exchange = 'SMART'
-contract.currency = 'USD'
-what_to_show = 'TRADES'
+        # set the data on the chart
+        if not df.empty:
+            chart.set(df)
 
-client.reqHistoricalData(
-    2, contract, '', '10 D', '1 min', what_to_show, True, 2, False, []
-)
-time.sleep(1)
+            # once we get the data back, we don't need a spinner anymore
+            #chart.spinner(False)
+
+def get_bar_data(symbol, timeframe):
+
+    print(f"getting bar data for {symbol} {timeframe}")
+
+    contract = Contract()
+    contract.symbol = symbol
+    contract.secType = 'STK'
+    contract.exchange = 'SMART'
+    contract.currency = 'USD'
+    what_to_show = 'TRADES'
+
+    client.reqHistoricalData(
+        2, contract, '', '10 D', timeframe, what_to_show, True, 2, False, []
+    )
+
+    time.sleep(1)
+
+    chart.watermark(symbol)
+
+if __name__ == '__main__':
+    client = IBClient(default_host, trading_port, default_client_id)
+    time.sleep(1)
+
+    chart = Chart(toolbox=True, width=1000, inner_width=1.0, inner_height=1)
+
+    get_bar_data(init_symbol, init_timeframe)
+
+    chart.show(block=True)
+    time.sleep(1)
